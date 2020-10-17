9340f65f6174dfbb91f50ebdf2c9cf75997d3f89
colorland\mpieprzyk <mpieprzyk@najlepszefoto.pl>
2020-10-17 18:08:41 +0200

    Additional tests

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { prefix } = require('config.json');
 
 client.once('ready', () => {
   console.log('Ready!');
-})
+});
 
-client.on('message', message => {
-  if (message.content === '!covid') {
-    message.channel.send('NIE ISTNIEJE');
+client.on('message', (message) => {
+  switch (message) {
+    case `${prefix}server`:
+      message.channel.send(`Ten serwer nazywa się: ${message.guild.name}`);
+      break;
+
+    case `${prefix}covid`:
+      message.channel.send('NIE ISTNIEJE');
+      break;
+
+    default:
+      break;
   }
 });
 
