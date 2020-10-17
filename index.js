0f84d6b8f1f38bfc8d5749b1f7ec2da1a3224898
colorland\mpieprzyk <mpieprzyk@najlepszefoto.pl>
2020-10-17 19:57:15 +0200

    config.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { prefix } = require('config.json');
+const { prefix } = require('./config.json');
 
 client.once('ready', () => {
   console.log('Ready!');
